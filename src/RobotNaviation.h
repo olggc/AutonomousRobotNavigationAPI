5c59492b49874de9230ecaa6c4b4db529612e94c
OLange <olange.guerson@gmail.com>
2021-04-20 23:05:36 -0300

    Feat: Create RobotNavigation Header

--- src/RobotNaviation.h
+++ src/RobotNaviation.h
@@ -1,26 +1,63 @@
 #ifndef DUMMY_H
 #define DUMMY_H
 #include <armadillo>
+#include <iostream>
+#include <string>	
 
 using namespace arma;
-/** This is a Robot Navigation class that generate all dynamics for simulation and tests purpose.
-* Dummy class with do_process function
+using namespace std;
+
+/** This is a Robot Navigation class for simulation and tests purpose.
+* The main purpose of this class is to generate all dynamics and sensoring for simulating in-game enviroment 
+* and test the efienciecy of kalman and particle filter for robot localization.
 */ 
 class RobotNavigation
 {
     private:
-        mat dynamic_matrix, controle_matrix, output_matrix;
-        mat path, landmarks_map;
+        mat dynamic_matrix, control_matrix, output_matrix;
+        /**
+         * dynamic matrix - the transition matrix containg system main dynamics
+         * control matrix - the control matrix containg influence of control dynamics on the system
+         * output matrix - the output matrix containg the dynamics of sensors measure
+         */
+        mat landmarks_map;
+        /**  
+         * landmarks map - a map containing all the land marks of the footbal field (X,L and T) intersections (x,y,type)
+         */ 
     protected:
 
     public:
-        mat state, aimed_state, control_vector, imu_data;
+        mat state, aimed_state, control_vector, path;
+        /**
+         * state - the robot actual state (x,y,th)
+         * aimed state - the robot desired state (x,y,th)
+         * control vector - vector containing the controllable variables of the system (v,w)
+         * imu data - matrix to load imu data collected from robot (ax,ay,wz)
+         */ 
         mat ball_position, landmarks_seen, robots_position;
+        /**
+         * ball position - a vector which contain the actual ball position (x,y)
+         * landmarks seen - a matrix containing the landmarks seen at the actual moment (x,y,type)
+         */ 
         float ellapsed_time, initial_time, time_step;
-        mat control_gains;
+        /**
+         * ellapsed time - ellapsed time since simulation start
+         * initial time - time which simulation started
+         * time step - discrete time step for simulation
+         */ 
+        mat control_gains_p, control_gains_pi, control_gains_pid;
+        /**
+         * control gain p - vector containing the gains for proportional control for pho, alpha and beta (kp,ka,kb)
+         * control gain pi - vector containing the gains for proportional-integral control for pho and alpha (kp,ka,ka_i)
+         * control gain pid - vector containing the gains for proportional-integral-derivative control for pho and alpha (kp,ka,ka_i,ka_d)
+         */ 
 
         RobotNavigation();
+        /** Constructor for RobotNavigation
+         */ 
         ~RobotNavigation();
+        /** Destructor for RobotNavigation
+         */ 
 
         void set_dynamic_matrix(mat dynamic_matrix);
         void set_control_matrix(mat controle_matrix);
@@ -32,7 +69,6 @@ class RobotNavigation
         void set_state(mat state);
         void set_aimed_state(mat aimed_state);
         void set_control_vector(mat control_vector);
-        void set_imu_data(mat imu_data);
 
         void set_ball_position(mat ball_position);
         void set_robots_position(mat robots_position);
@@ -41,12 +77,16 @@ class RobotNavigation
         void set_initial_time(float initial_time);
         void set_time_step(float time_step);
 
+        void load_imu_data(string imu_data_file);
+        void load_landmarks_map(string landmarks_map_file);
+
         mat get_state();
         mat get_aimed_state();
         mat get_control_vector();
         mat get_ball_position();
         mat get_landmarks_seen();
         mat get_robots_position();
+        mat get_ellapsed_time();
 
         mat control_p(mat aimed_state, mat control_gains);
         mat control_pi(mat aimed_state, mat control_gains);
@@ -54,7 +94,8 @@ class RobotNavigation
 
         mat path_planner(mat state, mat aimed_state, mat ball_position, mat robots_position);
         mat velocity_odometry(mat state, mat control_vector, float dt);
-        mat accelartion_odometry(mat state,mat imu_data, mat contrl_vector, float dt);
+        mat acceleration_odometry(mat state,mat imu_data, mat control_vector, float dt);
+        mat landmark_detection(mat state, mat landmarks_map);
 
 };
 
