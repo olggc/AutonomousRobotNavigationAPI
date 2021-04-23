c2dc734b6530eaf732abfce98f960d46cf35b637
OLange <olange.guerson@gmail.com>
2021-04-23 19:33:32 -0300

    Feat: Adding Functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,17 @@
 #include "RobotNavigation.h"
 
 using namespace std;
+using namespace arma;
 
 int main(int argc, const char** argv)
 {
+    RobotNavigation Lucy;
+
+    mat x;
+
+    x.zeros(3,1);
+
+    Lucy.set_state(x);
+
     cout << "RobotNavigation" << endl;
 }
